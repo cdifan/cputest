077bcb0db6c8ce46864f2a74555e2d8308eaa6f2
CD-i Fan <cdifan@gmail.com>
2021-08-02 09:30:00 +0200

    Convert into a CD-i system ROM image

--- cputest.bat
+++ cputest.bat
@@ -1,2 +1,7 @@
 r68 -q -n cputest.s -o=cputest.r -l >cputest.lst
 ren cputest.r cputest.r
+l68 -r=180000 cputest.r -o=cputest.rom -s >cputest18.map
+move cputest.rom cputest18.rom
+l68 -r=400000 cputest.r -o=cputest.rom -s >cputest40.map
+move cputest.rom cputest40.rom
+xcopy /y cputest*.rom ..\..\rom
